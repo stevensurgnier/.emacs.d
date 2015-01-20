795e5baacf202d31b1c59fed7a7081ab050a823b
Steven Surgnier <ssurgnier@gmail.com>
2015-01-20 22:10:56 +0000

    hang-in-there gif

--- user/packages/gif.el
+++ user/packages/gif.el
@@ -8,5 +8,6 @@
 (gif "its-happening" "http://media0.giphy.com/media/rl0FOxdz7CcxO/giphy.gif")
 (gif "woo" "http://www.reactiongifs.com/r/vhpy.gif")
 (gif "take-my-money" "http://www.keenandgraev.com/wp-content/uploads/2012/12/Take-my-Money-300x168.jpg")
+(gif "hang-in-there" "http://ecx.images-amazon.com/images/I/51S%2BpPtReeL._SY344_BO1,204,203,200_.jpg")
 
 (provide 'gif)
