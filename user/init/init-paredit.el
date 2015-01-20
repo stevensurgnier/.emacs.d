1a32518f18348b790f12ce7213d26a9f8de5622b
Steven Surgnier <ssurgnier@gmail.com>
2015-01-20 22:10:33 +0000

    indent fix in init-paredit.el

--- user/init/init-paredit.el
+++ user/init/init-paredit.el
@@ -1,7 +1,7 @@
 (dolist (hook '(clojure-mode-hook
-		emacs-lisp-mode-hook 
-		java-mode-hook
-		json-mode-hook
-		python-mode-hook
-		r-mode-hook))
+								emacs-lisp-mode-hook
+								java-mode-hook
+								json-mode-hook
+								python-mode-hook
+								R-mode-hook))
   (add-hook hook #'(lambda () (paredit-mode 1))))
