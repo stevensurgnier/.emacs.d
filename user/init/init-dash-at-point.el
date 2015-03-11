c7a932fc01ab5167a180e61a22fcc7e69adb60fa
Steven Surgnier <stevensurgnier@gmail.com>
2015-03-11 16:21:44 +0000

    indendation

--- user/init/init-dash-at-point.el
+++ user/init/init-dash-at-point.el
@@ -1,6 +1,6 @@
 (require 'dash-at-point)
 (add-to-list 'dash-at-point-mode-alist
-						 '(clojure-mode . "clojure"))
+             '(clojure-mode . "clojure"))
 
 (require 'steven-bindings)
 (define-key steven/bindings-map (kbd "C-c d") 'dash-at-point)
