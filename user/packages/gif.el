74fede80c3a80f946352008373db00d3f6ad3b4c
Steven Surgnier <stevensurgnier@gmail.com>
2015-04-11 05:03:40 +0000

    gif-toociced

--- user/packages/gif.el
+++ user/packages/gif.el
@@ -11,5 +11,6 @@
 (gif "hang-in-there" "http://ecx.images-amazon.com/images/I/51S%2BpPtReeL._SY344_BO1,204,203,200_.jpg")
 (gif "clap-wave-pow" "http://1.bp.blogspot.com/-ED_iMFF3A5I/UmBTCvjmpWI/AAAAAAAAaMA/wclkpl5n0ss/s1600/eQ8ECXX.gif")
 (gif "kanye-happy-clap" "http://soundisstyle.com/wp-content/uploads/2014/03/Kanye-West-Clapping.gif")
+(gif "toociced" "http://img.pandawhale.com/103113-thats-a-bingo-gif-Imgur-pyw2.gif")
 
 (provide 'gif)
