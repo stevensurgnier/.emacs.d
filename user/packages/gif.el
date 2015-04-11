75572d60d88bfee465556fa093f6f7ccb1097d35
Steven Surgnier <stevensurgnier@gmail.com>
2015-04-11 05:03:12 +0000

    gif-kanye-happy-clap

--- user/packages/gif.el
+++ user/packages/gif.el
@@ -10,5 +10,6 @@
 (gif "take-my-money" "http://www.keenandgraev.com/wp-content/uploads/2012/12/Take-my-Money-300x168.jpg")
 (gif "hang-in-there" "http://ecx.images-amazon.com/images/I/51S%2BpPtReeL._SY344_BO1,204,203,200_.jpg")
 (gif "clap-wave-pow" "http://1.bp.blogspot.com/-ED_iMFF3A5I/UmBTCvjmpWI/AAAAAAAAaMA/wclkpl5n0ss/s1600/eQ8ECXX.gif")
+(gif "kanye-happy-clap" "http://soundisstyle.com/wp-content/uploads/2014/03/Kanye-West-Clapping.gif")
 
 (provide 'gif)
